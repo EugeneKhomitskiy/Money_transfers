2e199d4c4b195e62d5abe802e4350c98341819b6
Eugene Khomitskiy <mtl_87@list.ru>
2021-06-14 14:15:56 +0300

    change money_transfers.kt

--- src/main/kotlin/money_transfers.kt
+++ src/main/kotlin/money_transfers.kt
@@ -1,13 +1,14 @@
 import kotlin.math.roundToInt
 
 const val RATE = 0.0075
+const val MIN_COMMISSION = 35.00
 
 fun main() {
     while (true) {
         println("Введите сумму перевода в рублях:")
         val amount = readLine()!!.toInt()
         val commission = amount * RATE
-        val result = (commission * 100.00).roundToInt() /100.00
+        val result = if (commission <= MIN_COMMISSION) 35.00 else (commission * 100.00).roundToInt() /100.00
         val str = result.toString().split(".").toTypedArray()
         val a = str[0]
         val b = str[1]
